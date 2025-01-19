0b47a6beb7db40da36e21ed8887ad5a25588e85d
Kyle Cheon <kcheon@my.bcit.ca>
2025-01-19 10:27:56 -0800

    refactor: improve folder image

--- src/components/TitleTag.tsx
+++ src/components/TitleTag.tsx
@@ -1,3 +1,6 @@
+import useMediaQuery from '@mui/material/useMediaQuery';
+import { useTheme } from '@mui/material/styles';
+
 type TagProps = {
   text?: string;
   large?: boolean;
@@ -5,15 +8,18 @@ type TagProps = {
 };
 
 export default function TitleTag({ text, large, medium }: TagProps) {
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
+
   return (
     <button
       disabled
       style={{
-        color: '#1B5074',
+        color: '#000000',
         backgroundColor: 'transparent',
         cursor: 'default',
         pointerEvents: 'none',
-        fontSize: '20px',
+        fontSize: isMobile ? '16px' : '20px',
       }}
     >
       {text}
